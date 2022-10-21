07e12a18b38ae98b1375afb1a87d36c47a99b008
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-10-21 20:30:30 -0300

    feat: req. 01 configured dockerFile, created all migrations and user model.

---
+++ app/backend/src/database/migrations/20221021191705-create-user.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ app/backend/src/database/migrations/20221021191707-create-matches.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/** @type {import('sequelize-cli'.Migration)} */
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        type:Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      home_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'home_team',
+        references : {
+          model: 'teams',
+          key: 'id'
+        }
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      away_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'away_team',
+        references: {
+          model: 'teams',
+          key: 'id',
+        }
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      in_progress: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+      }
+    },
+    {
+      timestamps: false,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matches');
+  }
+};
